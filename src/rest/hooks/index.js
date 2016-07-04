dca6f1979dcd78773f75891c8b0345ba8440dc34
Todd Geist <todd@geistinteractive.com>
2016-07-03 21:21:11 -0700

    add experimental support for WebSockets to SimpleRestService

--- src/rest/hooks/index.js
+++ src/rest/hooks/index.js
@@ -3,9 +3,28 @@
 
 const hooks = require('feathers-hooks');
 
+/*
+websockets requires passing in path vars on the query
+* so we move them off the query into the params
+* this might
+* */
+const socketfix = (hook)=>{
+  if(hook.params.provider === 'socketio'){
+
+    if(hook.params.query._db){
+      hook.params['_db']=hook.params.query._db;
+      delete hook.params.query._db
+    }
+
+    if(hook.params.query._lay){
+      hook.params['_lay']=hook.params.query._lay;
+      delete hook.params.query._lay
+    }
+  }
+};
 
 exports.before = {
-  all: [],
+  all: [socketfix],
   find: [],
   get: [],
   create: [],
